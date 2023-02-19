f4f3be4b764d3a8db6d5e0f2088c1ac0030107c6
Mohamed El Youssfi <mel-yous@e1r3p4.1337.ma>
2023-02-19 19:27:28 +0100

    Sorting small range of numbers is done

---
+++ tiny_sort.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tiny_sort.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 15:18:55 by mel-yous          #+#    #+#             */
+/*   Updated: 2023/02/18 23:34:20 by mel-yous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void sort_two_numbers(t_stack *stack_a)
+{
+	if (stack_a->num > stack_a->next->num)
+		swap(stack_a, "sa");
+}
+
+void sort_three_numbers(t_stack **stack_a)
+{
+	int median;
+
+	median = (*stack_a)->next->num;
+	if (median > (*stack_a)->num &&
+		median > (*stack_a)->next->next->num)
+	{
+		reverse_rotate(stack_a, "rra");
+		if ((*stack_a)->num > (*stack_a)->next->num)
+			swap(*stack_a, "sa");
+	}
+	else if (median < (*stack_a)->num &&
+		median < (*stack_a)->next->next->num)
+	{
+		if ((*stack_a)->num - (*stack_a)->next->next->num < 0)
+			swap(*stack_a, "sa");
+		else
+			rotate(stack_a, "ra");
+	}
+	else if (median < (*stack_a)->num &&
+		median > (*stack_a)->next->next->num)
+	{
+		swap(*stack_a, "sa");
+		reverse_rotate(stack_a, "rra");
+	}
+}
+
+void sort_four_numbers(t_stack **stack_a, t_stack **stack_b)
+{
+	int min_index;
+	
+	min_index = min(*stack_a)->index;
+	if (min_index == 1)
+		rotate(stack_a, "ra");
+	else if (min_index > 1)
+	{
+		while ((*stack_a)->index != min_index)
+			reverse_rotate(stack_a, "rra");
+	}
+	push(stack_a, stack_b, "pb");
+	sort_three_numbers(stack_a);
+	push(stack_b, stack_a, "pa");
+}
+
+void sort_five_numbers(t_stack **stack_a, t_stack **stack_b)
+{
+	int min_index;
+
+	min_index = min(*stack_a)->index;
+	if (min_index <= 2)
+		while ((*stack_a)->index != min_index)
+			rotate(stack_a, "ra");
+	else
+		while ((*stack_a)->index != min_index)
+			reverse_rotate(stack_a, "rra");
+	push(stack_a, stack_b, "pb");
+	re_index_stack(*stack_a);
+	sort_four_numbers(stack_a, stack_b);
+	push(stack_b, stack_a, "pa");
+}
