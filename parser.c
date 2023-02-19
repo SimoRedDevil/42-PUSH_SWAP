9d8a13839f0da15e9fcc64ea54a1953c8bd5c6d5
Mohamed El Youssfi <mel-yous@e1r3p4.1337.ma>
2023-02-19 19:24:20 +0100

    function named check_minus_plus is fixed, parsing is done

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 16:24:28 by mel-yous          #+#    #+#             */
-/*   Updated: 2023/02/17 20:06:19 by mel-yous         ###   ########.fr       */
+/*   Updated: 2023/02/19 18:25:36 by mel-yous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,19 @@ if true then throw an error.
 And check if there is a number before the sign, if true throw an error*/
 static void check_minus_plus(const char *str)
 {
-	while (*str)
+	int i;
+
+	i = 0;
+	while (str[i])
 	{
-		if (*str == '-' || *str == '+')
+		if (str[i] && (str[i] == '+' || str[i] == '-'))
 		{
-			if (!ft_isdigit(*(str + 1)) || ft_isdigit(*(str - 1)))
+			if (!ft_isdigit(str[i + 1]))
+				throw_error();
+			if (i > 0 && ft_isdigit(str[i - 1]))
 				throw_error();
 		}
-		str++;
+		i++;
 	}
 }
 
@@ -97,7 +102,7 @@ void	parse_input(const char **tab)
 		check_minus_plus(tab[i]);
 		check_numbers_only(tab[i]);
 		check_duplication(tab, tab[i], i);
-		ft_atoi(tab[i]);
+		//ft_atoi(tab[i]);
 		i++;
 	}
 	check_if_sorted(tab);
