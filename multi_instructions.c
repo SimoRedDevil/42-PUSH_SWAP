426f6633cbd8444af4d3e42726dcb203caa3aea3
Mohamed El Youssfi <mel-yous@e1r3p4.1337.ma>
2023-02-19 19:26:07 +0100

    Push swap instructions well done

---
+++ multi_instructions.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   multi_instructions.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 22:35:15 by mel-yous          #+#    #+#             */
+/*   Updated: 2023/02/16 16:03:33 by mel-yous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*Do sa and sb in the same time*/
+void ss(t_stack *stack_a, t_stack *stack_b)
+{
+	swap(stack_a, NULL);
+	swap(stack_b, NULL);
+	ft_putendl_fd("ss", 1);
+}
+
+/*Do ra and rb in the same time*/
+void rr(t_stack **stack_a, t_stack **stack_b)
+{
+	rotate(stack_a, NULL);
+	rotate(stack_b, NULL);
+	ft_putendl_fd("rr", 1);
+}
+
+/*Do rra and rrb in the same time*/
+void rrr(t_stack **stack_a, t_stack **stack_b)
+{
+	rotate(stack_a, NULL);
+	rotate(stack_b, NULL);
+	ft_putendl_fd("rrr", 1);
+}
---
+++ single_instructions.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   single_instructions.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mel-yous <mel-yous@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/14 16:26:07 by mel-yous          #+#    #+#             */
+/*   Updated: 2023/02/17 18:31:35 by mel-yous         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+/*Swap two first numbers of the following stack*/
+void swap(t_stack *stack, char *ins_name)
+{
+	int tmp;
+
+	if (!stack)
+		return ;
+	tmp = stack->num;
+	stack->num = stack->next->num;
+	stack->next->num = tmp;
+	ft_putendl_fd(ins_name, 1);
+}
+
+/*Take the first element at the top of source and put it at the top of destination*/
+void push(t_stack **source, t_stack **destination, char *ins_name)
+{
+    t_stack *tmp;
+
+    if (!source || !(*source))
+        return ;
+    tmp = *source;
+    if (!(*destination))
+    {
+        *source = (*source)->next;
+        tmp->next = NULL;
+        *destination = tmp;
+    }
+    else
+    {
+        *source = (*source)->next;
+        tmp->next = *destination;
+        (*destination) = tmp;
+    }
+	ft_putendl_fd(ins_name, 1);
+}
+
+/*The first element becomes the last one*/
+void rotate(t_stack **stack, char *ins_name)
+{
+    t_stack *head;
+    t_stack *sec;
+
+    if (!stack || !(*stack))
+        return ;
+    head = *stack;
+    sec = *stack;
+    while (sec->next)
+        sec = sec->next;
+    (*stack) = (*stack)->next;
+    sec->next = head;
+    head->next = NULL;
+	ft_putendl_fd(ins_name, 1);
+}
+
+/*The last element becomes the first one*/
+void reverse_rotate(t_stack **stack, char *ins_name)
+{
+	t_stack *head;
+	t_stack *sec;
+
+	if (!stack || !(*stack))
+		return ;
+	head = *stack;
+	sec = *stack;
+	while (sec->next->next)
+		sec = sec->next;
+	while (head->next)
+		head = head->next;
+	head->next = *stack;
+	*stack = head;
+	sec->next = NULL;
+	ft_putendl_fd(ins_name, 1);
+}
